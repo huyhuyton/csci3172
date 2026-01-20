7040592d0a10ed5dc9266fa9f3806e6f993f1c83
huyhuyton <th419943@dal.ca>
2026-01-20 00:54:36 -0400

    activities added

---
+++ activities/L4/js/script.js
@@ -0,0 +1,15 @@
+let name = "Diego Brando"; 
+let age = 232; 
+
+if (age <200) {
+    console.log(`Hello, my name is ${name}, I am ${age} years old and I am a student.`);
+}
+else {
+    console.log(`Hello, my name is ${name}, I am ${age} years old and I am not a student.`);
+}
+
+let newAge = age + 8; 
+
+let message = name + " will be " + newAge + " years old in 8 years.";
+
+console.log(message);
